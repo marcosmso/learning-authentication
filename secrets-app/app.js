3346db0e8745e58ac30e64eafbd5177d3b1e3e64
Marcos <marcosdp12@gmail.com>
2021-08-18 14:54:25 -0300

    Secrets App - Level 5 security - round salting hash

--- secrets-app/app.js
+++ secrets-app/app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -35,33 +36,38 @@ app.get("/login", (req, res) => {
 
 app.post("/register", (req, res) => {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function (err){
+            if(!err){
+                res.render("secrets");
+            } else {
+                console.log(err);
+            }
+        });
 
-    newUser.save(function (err){
-        if(!err){
-            res.render("secrets");
-        } else {
-            console.log(err);
-        }
     });
 
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result){
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
